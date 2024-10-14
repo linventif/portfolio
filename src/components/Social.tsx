3034829cec5aa10c9d6ada3c291dc7d410d11178
linventif <linventif@gmail.com>
2024-10-14 16:46:20 +0200

    Fix: A -> a

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -30,10 +30,10 @@ export const Social: Component = () => {
       <div class="flex flex-wrap items-center justify-center gap-6 w-full p-4">
         <For each={socialLinks}>
           {({ title, fontAwesomeIcon, href }) => (
-            <A href={href} class="btn btn-primary btn-outline">
+            <a href={href} class="btn btn-primary btn-outline">
               <i class={fontAwesomeIcon + " text-blue mr-2 text-lg"} />
               <span class="text-zinc-300 hover:text-white">{title}</span>
-            </A>
+            </a>
           )}
         </For>
       </div>
